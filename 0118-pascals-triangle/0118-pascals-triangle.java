94be8430d423ee1cd05e76df599e954d0f5ae01b
AYUSH-KUMAR-SINGH-28 <aks.singh662@gmail.com>
2026-07-31 23:48:25 +0530

    Time: 2 ms (10.88%), Space: 43.6 MB (16.15%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,6 +1,48 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+
         List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0; i<numRows; i++){
+            List<Integer> list = new ArrayList<>();
+
+            for(int j=0; j<=i; j++){
+               
+                    list.add(1);
+                                    
+                }
+                    ans.add(list);
+            }
+
+         for(int i=0; i<numRows; i++){
+            for(int j=0; j<=i; j++){
+                if(j==0 || j==i){
+                    ans.get(i).set(j,1);// arr[i][j] = 1
+                }
+                else{
+                    int val = ans.get(i-1).get(j) + ans.get(i-1).get(j-1); 
+                    // int val = arr[i-1][j] + arr[i-1][j-1]
+
+                    ans.get(i).set(j,val);
+
+                }
+            }
+        }
+        return ans;
+                
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    /*    List<List<Integer>> ans = new ArrayList<>();
         for(int i=0; i<numRows; i++){
             ans.add(new ArrayList<Integer>());
             for(int j=0; j<=i; j++){
@@ -18,6 +60,6 @@ class Solution {
         }
         return ans;
         //Time: O(n²)
-        //Space: O(n²)
+        //Space: O(n²)                               */
     }
 }
