f8d81bd2bfda93e78816ac53d8cde076cd94ced2
AYUSH KUMAR SINGH <aks.singh662@gmail.com>
2026-08-06 01:18:49 +0530

    Time: 0 ms (100%), Space: 43 MB (62.96%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,6 +1,51 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] arr) {
-        List<Integer> ans = new ArrayList<>();
+ List<Integer> ans = new ArrayList<>();
+        int m = arr.length; // Number of Rows
+        int n = arr[0].length; // Number of Column
+        int firstRow = 0;
+        int firstColumn = 0;
+        int lastRow = m-1;
+        int lastColumn = n-1;
+        int totalNumberOfElement = m*n;
+
+        while(ans.size()<totalNumberOfElement){
+
+            // Right
+            for(int j=firstColumn; j<=lastColumn; j++){
+                ans.add(arr[firstRow][j]);
+            }
+            firstRow++;
+
+            if(ans.size() == totalNumberOfElement) break;
+
+            //Down
+             for(int i=firstRow; i<=lastRow; i++){
+                ans.add(arr[i][lastColumn]);
+            }
+            lastColumn--;
+
+            if(ans.size() == totalNumberOfElement) break;
+
+            // Left
+              for(int j=lastColumn; j>=firstColumn; j--){
+                ans.add(arr[lastRow][j]);
+            }
+            lastRow--;
+            
+            if(ans.size() == totalNumberOfElement) break;
+
+            // Up
+              for(int i=lastRow; i>=firstRow; i--){
+                ans.add(arr[i][firstColumn]);
+            }
+            firstColumn++;
+        }
+        return ans;
+
+
+
+/*        List<Integer> ans = new ArrayList<>();
         int m = arr.length; // Number of Rows
         int n = arr[0].length; // Number of Column
         int firstRow = 0;
@@ -36,8 +81,7 @@ class Solution {
                 ans.add(arr[i][firstColumn]);
             }
             firstColumn++;
-            if(firstRow > lastRow || firstColumn > lastColumn) break;
         }
-        return ans;
+        return ans;    */
     }
 }
